7c7950e87285899ba3fe8ce0053d5b80f60e8154
xiao_en <1076763695@qq.com>
2024-10-09 16:09:03 +0800

    make and commit

--- lock/locker.h
+++ lock/locker.h
@@ -5,8 +5,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-class sem
-{
+class sem{
 public:
     sem(int num){
         if(sem_init(&_sem,0,num)!=0){
@@ -21,8 +20,57 @@ public:
         return sem_wait(&_sem)==0;
     }
     bool post(){
-
+        return sem_post(&_sem)==0;
     }
 private:
     sem_t _sem;
+};
+class locker{
+public:
+    locker(){
+        if (pthread_mutex_init(&_mutex,NULL)!=0){
+            throw std::exception();
+        }
+    }
+    ~locker(){
+        pthread_mutex_destroy(&_mutex);
+    }
+    bool lock(){
+        return pthread_mutex_lock(&_mutex)==0;
+    }
+    bool unlock(){
+        return pthread_mutex_unlock(&_mutex)==0;
+    }
+    pthread_mutex_t * get(){
+        return &_mutex;
+    }
+private:
+    pthread_mutex_t _mutex;
+};
+
+class cond{
+public:
+    cond(){
+        if (pthread_cond_init(&_cond,NULL)!=0){
+            throw std::exception();
+        }
+    }
+    ~cond(){
+        pthread_cond_destroy(&_cond);
+    }
+    bool wait(pthread_mutex_t *mutex){
+        int ret = 0;
+        ret = pthread_cond_wait(&_cond,mutex);
+        return ret == 0;
+    }
+    bool timewait(pthread_mutex_t *mutex,struct timespec t){
+        int ret = 0;
+        ret = pthread_cond_timedwait(&_cond,mutex,&t);
+        return ret==0;
+    }
+    bool signal(){
+        return pthread_cond_signal(&_cond)==0;
+    }
+private:
+    pthread_cond_t _cond;
 }
--- makefile
+++ makefile
@@ -7,9 +7,13 @@ else
     CXXFLAGS += -O2
 
 endif
-
-server: main.cpp  ./timer/lst_timer.cpp ./http/http_conn.cpp ./log/log.cpp ./CGImysql/sql_connection_pool.cpp  webserver.cpp config.cpp
+git:
+	@git add .
+	@git commit -m "make and commit"
+	@git push origin master
+server: main.cpp  ./timer/lst_timer.cpp ./http/http_conn.cpp ./log/log.cpp ./CGImysql/sql_connection_pool.cpp  webserver.cpp config.cpp git
 	$(CXX) -o server  $^ $(CXXFLAGS) -lpthread -lmysqlclient
+	
 
 clean:
 	rm  -r server
